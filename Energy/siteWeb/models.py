cc2f20be9ef9192bbb0b8455305e4c6c86ea458a
nicoleinae <nicoleinae@gmail.com>
2024-08-29 22:00:40 -0300

    feat: models

--- Energy/siteWeb/models.py
+++ Energy/siteWeb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=200, null=False)
+    email = models.CharField(max_length=200, unique=True)
+    senha = models.CharField(min_length=8, max_length=40)
+
+    def __str__(self):
+        return self.nome
