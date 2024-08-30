cb45bb508b16a61ecec34bb4ea3a26ba2145e3d5
nicoleinae <nicoleinae@gmail.com>
2024-08-30 20:49:52 -0300

    feat: conculta em models

--- Energy/siteWeb/models.py
+++ Energy/siteWeb/models.py
@@ -2,8 +2,24 @@ from django.db import models
 
 class Usuario(models.Model):
     nome = models.CharField(max_length=200, null=False)
-    email = models.CharField(max_length=200, unique=True)
-    senha = models.CharField(null=False, max_length=40)
+    email = models.EmailField(max_length=254, unique=True)  
+    senha = models.CharField(max_length=40, null=False)
 
     def __str__(self):
         return self.nome
+
+class Consulta(models.Model):
+    TIPOS_IMOVEL = [
+        ("R", "Residencial"),
+        ("E", "Empresarial"),
+    ]
+
+    data = models.DateField(null=False)
+    cep = models.CharField(max_length=8, null=False)  
+    valorContaLuz = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    ajusteTarifa = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    tipoImovel = models.CharField(max_length=1, choices=TIPOS_IMOVEL, null=False)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Consulta {self.id} - {self.tipoImovel} em {self.data}"
--- Energy/siteWeb/views.py
+++ Energy/siteWeb/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
-#importando modelos e serializers
 from .models import Usuario
 from .serializers import UsuarioSerializer
 
