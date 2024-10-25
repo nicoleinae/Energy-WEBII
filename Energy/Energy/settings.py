cc39fab19bdf457447b3d75750454e3ea650e718
Emerson Bottega <126634857+EmersonBottega@users.noreply.github.com>
2024-10-25 22:59:54 +0000

    feat: configuração do django core headers

--- Energy/Energy/settings.py
+++ Energy/Energy/settings.py
@@ -30,9 +30,12 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 INSTALLED_APPS = [
     'siteWeb.apps.SitewebConfig',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
