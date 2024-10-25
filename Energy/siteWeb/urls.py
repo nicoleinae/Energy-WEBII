064655eeb2178382cd3ccc779255c8051aa8096b
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2024-10-25 23:03:14 +0000

    feat: consulta urls

--- Energy/siteWeb/urls.py
+++ Energy/siteWeb/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import UsuarioView, UsuarioReadUpdateDeleteView
+from .views import (
+    UsuarioView, 
+    UsuarioReadUpdateDeleteView, 
+    ConsultaView, 
+    ConsultaReadDeleteView
+)
 
 urlpatterns = [
     path('usuario/', UsuarioView.as_view()),
-    path('usuario/<int:pk>/', UsuarioReadUpdateDeleteView.as_view(), name='professor-detail'),
+    path('usuario/<int:pk>/', UsuarioReadUpdateDeleteView.as_view(), name='usuario-detail'),
 
+    path('consulta/', ConsultaView.as_view()),
+    path('consulta/<int:pk>/', ConsultaReadDeleteView.as_view(), name='consulta-detail'),
 ]
