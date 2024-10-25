947647f9ba7a7c353bf843c704628d98509ee4e9
nicoleinae <nicoleinae@gmail.com>
2024-10-25 20:10:36 -0300

    feat: serializers consulta

--- Energy/siteWeb/serializers.py
+++ Energy/siteWeb/serializers.py
@@ -4,4 +4,14 @@ from .models import Usuario
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
+        fields = ['id', 'nome', 'email', 'senha']
+                
+class ConsultaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = 'id', 'data', 'cep', 'valorContaLuz', 'ajusteTarifa', 'tipoImovel', 'usuário'
+        
+class ConsultaReadDeleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
         fields = '__all__'
